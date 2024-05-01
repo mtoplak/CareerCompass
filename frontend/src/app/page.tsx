b3fb0bd8f26ad2358a386f0ef35f0bc65a7ebef7
Elena <elena.ugwoegbulam@student.um.si>
2024-05-01 18:11:37 +0200

    Change the landing page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,16 +1,7 @@
-import About from "@/components/About";
-import HomeBlogSection from "@/components/Blog/HomeBlogSection";
 import CallToAction from "@/components/CallToAction";
-import Clients from "@/components/Clients";
 import ScrollUp from "@/components/Common/ScrollUp";
-import Contact from "@/components/Contact";
-import Faq from "@/components/Faq";
-import Features from "@/components/Features";
 import LandingPageRate from "@/components/LandingPageRate";
 import Hero from "@/components/Hero";
-import Pricing from "@/components/Pricing";
-import Team from "@/components/Team";
-import Testimonials from "@/components/Testimonials";
 import { getAllPosts } from "@/utils/markdown";
 import { Metadata } from "next";
 import LandingPageJobs from "@/components/LandingPageJobs";
@@ -21,16 +12,29 @@ export const metadata: Metadata = {
     "CareerCompass is a platform that helps job seekers to find the right company to work for. It provides a list of companies with their ratings and reviews. Users can also look for job advertisements and chat with an AI chatbot to get career advice.",
 };
 
-export default function Home() {
+//potrebno spremenit funkcijo za najbolje ocenjene ko se naredi
+async function getCompanies() {
+  const res = await fetch(`http://localhost:4000/company`, {
+    cache: "no-store",
+  });
+  const companies = await res.json();
+
+  return companies;
+}
+
+const Home = async () => {
   const posts = getAllPosts(["title", "date", "excerpt", "coverImage", "slug"]);
+  const companies = await getCompanies();
 
   return (
     <main>
       <ScrollUp />
       <Hero />
-      <LandingPageRate />
+      <LandingPageRate companies={companies} />
       <CallToAction />
       <LandingPageJobs />
     </main>
   );
 }
+
+export default Home;
