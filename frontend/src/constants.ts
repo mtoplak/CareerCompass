e94d39ca2e0be021361284bffc87f18aac0cddf7
Tia <tia.zvajker@student.um.si>
2024-05-09 20:24:00 +0200

    Add modules

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,3 +1,3 @@
-export const api = process.env.NODE_ENV === 'development'
-    ? process.env.NEXT_PUBLIC_API_URL
-    : 'http://localhost:4000/';
+export const api = process.env.NODE_ENV == 'development'
+    ? 'http://localhost:4000'
+    : process.env.NEXT_PUBLIC_API_URL;
