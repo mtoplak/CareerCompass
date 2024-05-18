e5f86f337c1e0d229b0adaf46e8c786d9ea5c174
Tia <tia.zvajker@student.um.si>
2024-05-18 19:31:34 +0200

    Include job advertisements in company DTO

--- backend/src/db/entities/job-advertisement.model.ts
+++ backend/src/db/entities/job-advertisement.model.ts
@@ -7,7 +7,6 @@ export const JobAdvertisementSchema = new Schema({
   city: { type: String, required: false },
   company_linked: { type: Schema.Types.ObjectId, ref: 'Company' },
   company: { type: String },
-  salary: { type: String },
   url: { type: String, required: false },
   source: { type: String, required: false },
 });
@@ -18,7 +17,6 @@ export interface JobAdvertisement extends Document {
   city: string; // kraj
   company_linked: Company; // podjetje
   company: string; // podjetje
-  salary: string; // placa
   url: string; // url do oglasa
   source: string; // vir
 }
